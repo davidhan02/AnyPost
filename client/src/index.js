54d87f6194187b76d6f0040266ac7130a4f6a24b
David Han <davidhan.5.25@gmail.com>
2019-05-10 21:00:50 -0700

    Added client dependencies and set up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './utils/store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
