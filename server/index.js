0e6cca1fb568a610af02bc5b45197d9dedcf3aed
David Han <davidhan.5.25@gmail.com>
2019-05-10 22:13:00 -0700

    Try 5

--- server/index.js
+++ server/index.js
@@ -8,14 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(
-        'https://tranquil-citadel-17264.herokuapp.com',
-        'client',
-        'build',
-        'index.html'
-      )
-    );
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
