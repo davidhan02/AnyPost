09f1441d494b95078e2c794a1a4367faf5dee8de
David Han <davidhan.5.25@gmail.com>
2019-05-10 22:50:25 -0700

    Refactor App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import history from '../utils/history';
+import React from 'react';
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-class App extends Component {
-  render() {
-    return (
-      <Router history={history}>
-        <>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/surveys" component={SurveyNew} />
-            <Route path="/dashboard" component={Dashboard} />
-          </Switch>
-        </>
-      </Router>
-    );
-  }
-}
+const App = props => (
+  <Router history={history}>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/surveys" component={SurveyNew} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </>
+  </Router>
+);
 
 export default App;
