d162b83fa74b6380ec1f072817c27ef9f25264d5
David Han <davidhan.5.25@gmail.com>
2019-05-10 18:12:23 -0700

    Fixed production

--- server/index.js
+++ server/index.js
@@ -10,11 +10,4 @@ app.get('/surveys', (req, res) => {
   res.send('Logged in');
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(process.env.PORT || 5000);
