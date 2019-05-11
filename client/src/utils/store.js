7b326aa80d179fb79470d19a82658482dda23372
David Han <davidhan.5.25@gmail.com>
2019-05-11 16:19:11 -0700

    Set up theme client middleware that sets localStorage accordingly

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -2,13 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { combineReducers } from 'redux';
 import auth from '../reducers/auth';
 import theme from '../reducers/theme';
+import themeMiddleware from '../middleware/theme';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combineReducers({ auth, theme }),
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, themeMiddleware))
 );
 
 export default store;
