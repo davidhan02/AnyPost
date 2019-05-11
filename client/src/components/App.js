f3a4212d82c0d1c6d41a541be9f05824f206a3a7
David Han <davidhan.5.25@gmail.com>
2019-05-10 21:15:45 -0700

    Created history for use in Router and added test components for Router testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import history from '../utils/history';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Router history={history}>
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/surveys" component={SurveyNew} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </>
+      </Router>
     );
   }
 }
