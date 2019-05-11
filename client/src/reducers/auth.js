e6ac55d2be5415b1f17298a63621da9c782f9678
David Han <davidhan.5.25@gmail.com>
2019-05-11 16:15:01 -0700

    Set up recieved actions in reducers

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,18 @@
+import { FETCH_USER } from '../actions/types';
+import isEmpty from '../utils/isEmpty';
+
 const initialState = {
   isAuthenticated: false,
-  loading: false,
   user: {}
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return {
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
