bff93669aa2cda7a5275cf69ead93e0168217d49
David Han <davidhan.5.25@gmail.com>
2019-05-11 14:17:00 -0700

    Slight refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import './utils/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './utils/store';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-
-import store from './utils/store';
 
 ReactDOM.render(
   <Provider store={store}>
