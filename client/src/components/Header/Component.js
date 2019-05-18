6f2feb6e841bdf2bbbf2becd90c9e86b84e2524a
David Han <davidhan.5.25@gmail.com>
2019-05-17 19:50:54 -0700

    Refactor HeaderWrapper into its own file

--- client/src/components/Header/Component.js
+++ client/src/components/Header/Component.js
@@ -2,51 +2,42 @@ import React from 'react';
 import HeaderLogo from './Logo';
 import styled from 'styled-components/macro';
 import HeaderDarkButton from './DarkButton';
+import HeaderWrapper from './Wrapper';
 import HeaderNavLink from './NavLink';
 import HeaderUsername from './Username';
 
-const Wrapper = styled.header`
-  position: sticky;
-  z-index: 10;
-  top: 0;
+const InnerWrapper = styled.div`
+  width: 100%;
+  height: 48px;
+  margin: 0 auto;
+  max-width: 1050px;
 
   display: flex;
   align-items: stretch;
-  margin-bottom: 20px;
-
-  box-shadow: 0 4px 12px ${props => props.theme.shadow};
-  border-bottom: 1px solid ${props => props.theme.border};
-
-  height: 48px;
-  padding: 0 10vw;
-  background-color: ${props => props.theme.foreground};
   user-select: none;
 
   @media (max-width: 425px) {
-    margin-bottom: 16px;
     height: 40px;
-  }
-
-  @media (max-width: 768px) {
-    padding: 0;
   }
 `;
 
 const Header = ({ auth, logout, toggleTheme }) => (
-  <Wrapper>
-    <HeaderLogo />
-    <HeaderDarkButton onClick={toggleTheme} />
-    {auth.isAuthenticated ? (
-      <>
-        <HeaderUsername user={auth.user} />
-        <HeaderNavLink as="span" onClick={logout}>
-          Logout
-        </HeaderNavLink>
-      </>
-    ) : (
-      <HeaderNavLink to="/login">Login</HeaderNavLink>
-    )}
-  </Wrapper>
+  <HeaderWrapper>
+    <InnerWrapper>
+      <HeaderLogo />
+      <HeaderDarkButton onClick={toggleTheme} />
+      {auth.isAuthenticated ? (
+        <>
+          <HeaderUsername user={auth.user} />
+          <HeaderNavLink as="span" onClick={logout}>
+            Logout
+          </HeaderNavLink>
+        </>
+      ) : (
+        <HeaderNavLink to="/login">Login</HeaderNavLink>
+      )}
+    </InnerWrapper>
+  </HeaderWrapper>
 );
 
 export default Header;
