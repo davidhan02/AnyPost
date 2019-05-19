8c283a38d81f09db7dbc2a11106fbac577a6877e
David Han <davidhan.5.25@gmail.com>
2019-05-18 21:17:55 -0700

    Updated api routes with posts routes for listing posts

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,15 @@
 const users = require('./controllers/users');
+const posts = require('./controllers/posts');
 const router = require('express').Router();
 
 router.post('/login', users.loginUser);
 router.get('/logout', users.logoutUser);
 router.post('/register', users.register);
 router.get('/current_user', users.currentUser);
+
+router.get('/posts', posts.listAll);
+router.get('/user/:userId', posts.listByUserId);
+router.get('/posts/:category', posts.listByCategory);
 
 module.exports = app => {
   app.use('/api', router);
