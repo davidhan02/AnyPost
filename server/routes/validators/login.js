7413e1c7d9992c65abf78ecae3a07b5e9c34c2d3
David Han <davidhan.5.25@gmail.com>
2019-05-23 18:32:22 -0700

    Fixed login validator

--- server/routes/validators/login.js
+++ server/routes/validators/login.js
@@ -4,7 +4,6 @@ const isEmpty = require('./isEmpty');
 module.exports = function loginValidator(data) {
   let errors = {};
 
-  data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
 
@@ -14,14 +13,6 @@ module.exports = function loginValidator(data) {
 
   if (Validator.isEmpty(data.email)) {
     errors.msg = 'Email field is required';
-  }
-
-  if (Validator.isEmpty(data.name)) {
-    errors.msg = 'Name field is required';
-  }
-
-  if (!Validator.isLength(data.name, { min: 2, max: 25 })) {
-    errors.msg = 'Name must be between 2 and 25 characters';
   }
 
   if (Validator.isEmpty(data.password)) {
