8a58aacc5d0bd2d7e99150527273bf6ef50fb95f
David Han <davidhan.5.25@gmail.com>
2019-06-08 16:41:00 -0700

    Added voting to profile Schema

--- server/models/profile/index.js
+++ server/models/profile/index.js
@@ -4,11 +4,12 @@ const Schema = mongoose.Schema;
 const expSchema = require('./experience');
 const eduSchema = require('./education');
 const socialSchema = require('./social');
+const voteSchema = require('../vote');
 
 const profileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
+    ref: 'User'
   },
   date: {
     type: Date,
@@ -38,6 +39,7 @@ const profileSchema = new Schema({
   bio: {
     type: String
   },
+  votes: [voteSchema],
   experience: [expSchema],
   education: [eduSchema],
   social: socialSchema
@@ -50,6 +52,47 @@ profileSchema.options.toJSON.transform = (doc, ret) => {
   delete obj.__v;
   return obj;
 };
+
+profileSchema.pre(/^find/, function() {
+  this.populate('user');
+});
+
+profileSchema.pre('save', function(next) {
+  this.wasNew = this.isNew;
+  next();
+});
+
+profileSchema.virtual('upvotePercentage').get(function() {
+  if (this.votes.length === 0) return 0;
+  const upvotes = this.votes.filter(({ vote }) => vote === 1);
+  return Math.floor((upvotes.length / this.votes.length) * 100);
+});
+
+profileSchema.methods.vote = function(user, vote) {
+  const existingVote = this.votes.find(item => item.user._id.equals(user));
+
+  if (existingVote) {
+    this.score -= existingVote.vote;
+    if (vote === 0) {
+      this.votes.pull(existingVote);
+    } else {
+      this.score += vote;
+      existingVote.vote = vote;
+    }
+  } else if (vote !== 0) {
+    this.score += vote;
+    this.votes.push({ user, vote });
+  }
+  return this.save();
+};
+
+profileSchema.post('save', function(doc, next) {
+  if (this.wasNew) this.vote(this.user._id, 1);
+  doc
+    .populate('user')
+    .execPopulate()
+    .then(() => next());
+});
 
 const Profile = mongoose.model('Profile', profileSchema);
 
