1225c1525005269d75557e236e19b76842428dfb
David Han <davidhan.5.25@gmail.com>
2019-06-08 16:21:15 -0700

    Started creation of Profile model

---
+++ server/models/profile/index.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const expSchema = require('./experience');
+const eduSchema = require('./education');
+const socialSchema = require('./social');
+
+const profileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  interests: {
+    type: [String]
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  experience: [expSchema],
+  education: [eduSchema],
+  social: socialSchema
+});
+
+profileSchema.set('toJSON', { getters: true });
+profileSchema.options.toJSON.transform = (doc, ret) => {
+  const obj = { ...ret };
+  delete obj.id;
+  delete obj.__v;
+  return obj;
+};
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
